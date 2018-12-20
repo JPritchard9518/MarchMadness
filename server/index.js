05d925f247daf298c5dcab68262e8cfcbca0d221
JPritchard9518 <jpritchard9518@gmail.com>
2018-12-19 21:57:07 -0600

    Push before deploy

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 
 
